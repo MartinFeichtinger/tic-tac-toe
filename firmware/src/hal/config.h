8aeba255c09903095d603b017360e87aebff3f5f
Martin Feichtinger <martin.feichtinger22@gmail.com>
2026-02-27 17:36:56 +0100

    added hal_gpio.h with macro mapping for the basic gpio function. therfor the config.h had also be adapted

--- firmware/src/hal/config.h
+++ firmware/src/hal/config.h
@@ -1,25 +1,22 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// µControler specs: ATtiny461A, RAM=4KB, CPU=20MHz
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
+// µControler specs: ATtiny461A, Flash=4kB, RAM=256Bytes, CPU=20MHz
 
 // digital output pins
-#define ROW0_GREEN	    PA0
-#define ROW1_GREEN	    PA2
-#define ROW2_GREEN	    PA4
-#define ROW0_RED	    PA1
-#define ROW1_RED	    PA3
-#define ROW2_RED	    PA5
-#define COL0		    PB4
-#define COL1		    PB5
-#define COL2		    PB6
-#define SPEAKER		    PB3
+#define ROW0_GREEN	    0, A    //PA0
+#define ROW1_GREEN	    2, A    //PA2
+#define ROW2_GREEN	    4, A    //PA4
+#define ROW0_RED	    1, A    //PA1
+#define ROW1_RED	    3, A    //PA3
+#define ROW2_RED	    5, A    //PA5
+#define COL0		    4, B    //PB4
+#define COL1		    5, B    //PB5
+#define COL2		    6, B    //PB6
+#define SPEAKER		    3, B    //PB3
 
 // analog input pins
-#define PLAY_BUTTONS	PA6		// two buttons at one pin
-#define MODE_SWITCHES   PA7		// two switches at one pin
+#define PLAY_BUTTONS	6, A	//PA6	// two buttons at one pin
+#define MODE_SWITCHES   7, A	//PA7	// two switches at one pin
 
 #endif
