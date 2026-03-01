72a309413b76db7c60d76e823f78722f514a7e9a
Martin Feichtinger <mfeichtinger@student.tugraz.at>
2026-03-01 23:35:44 +0100

    added driver for the speaker. the SPEAKER_system_tick() isn't implemented yet

--- firmware/src/hal/hal_timer.h
+++ firmware/src/hal/hal_timer.h
@@ -1,11 +1,13 @@
 #ifndef HAL_TIMER_H
 #define HAL_TIMER_H
 
-#include <avr/interrupt.h>
+#include <stdint.h>
 
 // Timer 0: system tick generator
 void TIMER_init_timer_0(void);
 
 // Timer 1: pwm generator for the speaker
+void TIMER_init_timer_1(void);
+void TIMER_set_timer_1_frequency(uint16_t frequency);	// frequency range: 62Hz - 15,625kHz, set to 0 to stop the timer
 
 #endif
