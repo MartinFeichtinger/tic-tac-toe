cc50523c57b9cb134b685a72f045022ab50f2571
Martin Feichtinger <martin.feichtinger22@gmail.com>
2026-03-02 00:18:40 +0100

    created a test for the speaker driver

---
+++ firmware/src/tests/post/speaker_test.c
@@ -0,0 +1,25 @@
+#include "driver_speaker.h"
+#include <stdint.h>
+#include "hal_timer.h"
+#include "hal_time.h"
+
+void system_ticks_callback_handler(void);
+
+int main(void)
+{
+	TIMER_init_timer_0();
+	SPEAKER_init();
+	TIME_set_system_ticks_callback(system_ticks_callback_handler);
+
+	for(uint16_t freq=100; freq<=1500; freq+=100)
+	{
+		SPEAKER_play_sound(freq, 300);
+		TIME_delay_ms(500);
+	}
+}
+
+
+void system_ticks_callback_handler(void)
+{
+	SPEAKER_system_tick();
+}
