0f61373c84c2d896d7e96355f26e83d21993fd14
Martin Feichtinger <mfeichtinger@student.tugraz.at>
2026-03-01 17:40:08 +0100

    increads F_CPU to 8MHz

--- firmware/src/hal/hal_timer.c
+++ firmware/src/hal/hal_timer.c
@@ -8,10 +8,10 @@ void TIMER_init_timer_0(void)
 
 	cli();				// deactivate interrupts
 
-	TCCR0A=(1<<0);		// set timer0 to 8 bit ouput compare mode (CTC0-bit but it seems like the header file cant find it => hardcoded / datasheet page 83)
-	OCR0A=124;			// set compare value to 124
-	TIMSK|=(1<<OCIE0A);	// activate timer0 output compare interrupt
-	TCCR0B=(1<<CS01);	// prescaler=8 and start timer
-	
+	TCCR0A=(1<<0);				// set timer0 to 8 bit ouput compare mode (CTC0-bit but it seems like the header file cant find it => hardcoded / datasheet page 83)
+	OCR0A=124;					// set compare value to 124
+	TIMSK|=(1<<OCIE0A);			// activate timer0 output compare interrupt
+	TCCR0B=(1<<CS01)|(1<<CS00);	// prescaler=64 and start timer
+
 	sei();				// activate interrupts
 }
