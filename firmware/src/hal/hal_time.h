07922fa0dcfdffe07fdd11ea6bd3f6ae39aa3560
Martin Feichtinger <mfeichtinger@student.tugraz.at>
2026-03-01 17:47:10 +0100

    added delay function to hal_time.h

--- firmware/src/hal/hal_time.h
+++ firmware/src/hal/hal_time.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 
 uint32_t TIME_get_millis(void);
+void TIME_delay_ms(uint16_t ms);
 
 typedef void (*TIME_system_ticks_callback_t)(void);
 void TIME_set_system_ticks_callback(TIME_system_ticks_callback_t callback);
