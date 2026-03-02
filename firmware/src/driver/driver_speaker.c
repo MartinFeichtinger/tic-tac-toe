f3a2679bb513b1c669c04ff6d635baa6fa712b22
Martin Feichtinger <mfeichtinger@student.tugraz.at>
2026-03-02 19:38:42 +0100

    changed speaker driver back to basic setting. melodys should be played later via an special music.h lib

--- firmware/src/driver/driver_speaker.c
+++ firmware/src/driver/driver_speaker.c
@@ -14,9 +14,9 @@ void SPEAKER_init(void)
 }
 
 
-void SPEAKER_play_sound(musical_note_t note, uint16_t duration_ms)
+void SPEAKER_play_sound(uint16_t frequncy, uint16_t duration_ms)
 {
-	TIMER_set_timer_1_frequency(note);
+	TIMER_set_timer_1_frequency(frequncy);
 	duration_ms_countdown = duration_ms;
 }
 
