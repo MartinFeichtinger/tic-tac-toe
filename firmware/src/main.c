923552b9f6556b61d520046ec6a6c97ae486c19d
Martin Feichtinger <martin.feichtinger22@gmail.com>
2026-03-18 21:24:44 +0100

    updated main to init all drivers

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,6 +3,8 @@
 #include "driver_led_display.h"
 #include "driver_speaker.h"
 #include "driver_music.h"
+#include "driver_mode_switches.h"
+#include "driver_play_buttons.h"
 
 #include "app_post_animation.h"
 
@@ -15,8 +17,11 @@ int main(void)
 	// many driver inits
 	TIME_init();
 	TIME_set_system_ticks_callback(system_ticks_callback_handler);
+
 	LED_DISPLAY_init();
 	SPEAKER_init();
+	MODE_SWITCHES_init();
+	PLAY_BUTTONS_init();
 
 	// run post animation
 	post_animation();
@@ -33,4 +38,5 @@ void system_ticks_callback_handler(void)
 	LED_DISPLAY_system_tick();
 	SPEAKER_system_tick();
     MUSIC_system_tick();
+	PLAY_BUTTONS_system_tick();
 }
